cca81234003ec14fb1a24dea46b4bab81b16ff52
Nikityau <kapitnov@list.ru>
2022-07-04 15:30:44 +0300

    project page Safari 2, forgot few gradients

--- src/components/Pages/ProjectPage/Preivew.block/Back.part/Back.part.tsx
+++ src/components/Pages/ProjectPage/Preivew.block/Back.part/Back.part.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import './Back.part.scss'
 
 import back_img from '../../../../../assets/images/syntetic-project-back.png'
 
+import {AppContext, Browser} from "../../../../App/App";
+
 const BackPart = () => {
+    const appContext = useContext(AppContext)
     return (
         <div className={'back-part'}
         style={{ backgroundImage: `url(${back_img})` }}>
-            {/*<div className={'back-part__gradient-pink'}/>*/}
+            <div className={
+                appContext.browser !== Browser.SAFARI
+                    ? 'back-part__gradient-pink_Safari'
+                    : 'back-part__gradient-pink_Default'
+            }/>
             <div className={'back-part__blur'} />
         </div>
     );
